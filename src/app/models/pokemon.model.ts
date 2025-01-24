5d8f4fd3d5c7143c4befda6f5ebbb91ff037b9fd
Rashy-hub <yagoubi.r@gmail.com>
2025-01-24 10:15:48 +0100

    feat(model): create model class for Pokemon

---
+++ src/app/models/pokemon.model.ts
@@ -0,0 +1,12 @@
+import { PokemonType } from '../utils/pokemon.utils'
+
+export class Pokemon {
+    name: string = 'Pokemon'
+    image: string = '/default.png'
+    type: PokemonType = PokemonType.Electric
+    hp: number = 0
+    figureCaption: string = 'figure desc'
+    attackName: string = 'standard attack'
+    attackStrength: number = 0
+    attackDescription: string = 'attack description ...'
+}
---
+++ src/app/utils/pokemon.utils.ts
@@ -0,0 +1,10 @@
+// enum qui associe monster.type à une couleur
+
+enum PokemonType {
+    Fire = 'fire',
+    Water = 'water',
+    Plant = 'plant',
+    Electric = 'electric',
+}
+
+export { PokemonType }
