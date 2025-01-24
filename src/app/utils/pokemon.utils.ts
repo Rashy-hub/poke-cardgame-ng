ba5502fe403b9ff84fc08b5a8ff3a093d0ff6d37
Rashy-hub <yagoubi.r@gmail.com>
2025-01-24 10:44:07 +0100

    feat (utils) : create enums for pokemon elements

--- src/app/utils/pokemon.utils.ts
+++ src/app/utils/pokemon.utils.ts
@@ -7,4 +7,17 @@ enum PokemonType {
     Electric = 'electric',
 }
 
-export { PokemonType }
+enum BackgroundColor {
+    Red = 'rgb(255, 99, 71)', // Tomate,
+    Blue = 'rgb(70, 130, 180)', // SteelBlue,
+    Green = 'rgb(60, 179, 113)', // MediumSeaGreen,
+    Yellow = 'rgb(255, 215, 0)', // Gold
+}
+
+const typeToBackgroundColorMap = {
+    [PokemonType.Fire]: BackgroundColor.Red,
+    [PokemonType.Water]: BackgroundColor.Blue,
+    [PokemonType.Plant]: BackgroundColor.Green,
+    [PokemonType.Electric]: BackgroundColor.Yellow,
+}
+export { PokemonType, BackgroundColor, typeToBackgroundColorMap }
