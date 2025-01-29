66ec4fc9b92b02efe243439535d48657a1e6eb91
Rashy-hub <yagoubi.r@gmail.com>
2025-01-29 16:57:57 +0100

    feat (routes) : update routes (lazy loading) and create main pages components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,31 @@
 import { Routes } from '@angular/router'
+import { authGuard } from './guards/auth.guard'
 
 export const routes: Routes = [
     {
         path: '',
-        loadComponent: () =>
-            import(
-                './pages/pokemons-list-page/pokemons-list-page.component'
-            ).then((m) => m.PokemonsListPageComponent),
+        redirectTo: 'pokemons',
         pathMatch: 'full',
     },
+    {
+        path: 'pokemons',
+        loadComponent: () => import('./pages/pokemons-list-page/pokemons-list-page.component').then((m) => m.PokemonsListPageComponent),
+        canActivate: [authGuard],
+        // resolve: { pokemons: () => import('./services/pokemon.service').then((m) => m.PokemonService) },
+    },
+    {
+        path: 'pokemon/create',
+        loadComponent: () => import('./pages/pokemon/pokemon-page.component').then((m) => m.PokemonPageComponent),
+        canActivate: [authGuard],
+    },
+    {
+        path: 'pokemon/:id',
+        loadComponent: () => import('./pages/pokemon/pokemon-page.component').then((m) => m.PokemonPageComponent),
+        canActivate: [authGuard],
+    },
+    {
+        path: 'login',
+        loadComponent: () => import('./pages/login-page/login-page.component').then((m) => m.LoginPageComponent),
+    },
+    { path: '**', redirectTo: '' }, // Gérer les chemins inconnus
 ]
